b517df42ee6334da308b2f0fea8432d4f06b8fc2
abhijith <abijithag2244@gmail.com>
2026-01-03 00:39:31 +0530

    episode1

---
+++ app.js
@@ -0,0 +1,11 @@
+  const header = React.createElement("div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},
+     [React.createElement("h1",{},"Hello World"),
+     React.createElement('h2',{},"Hello World 2")
+     ]
+    )
+    )
+
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+    root.render(header)
