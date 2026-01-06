a6cf2f8185ca8209d324045052f228050a86c073
abhijith <abijithag2244@gmail.com>
2026-01-07 00:53:34 +0530

    remove node_modules

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-  const header = React.createElement("div",
-    {id:"parent"},
-    React.createElement("div",{id:"child"},
-     [React.createElement("h1",{},"Hello World"),
-     React.createElement('h2',{},"Hello World 2")
-     ]
-    )
-    )
 
-    const root = ReactDOM.createRoot(document.getElementById('root'))
-    root.render(header)
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+const elem = <span>React elemnt</span>
+
+
+const titile = (
+  <h1>
+  
+  Namste React 2
+    {elem}
+  </h1>
+
+)
+
+
+
+
+
+  const HeadingComponent = ()=>(
+  
+      <div  id="container">
+        {titile}
+        <h1 className="heading"> Namste React</h1>
+      </div>
+    )
+  
+
+const jsxHeading = <h1 id="header">Namaste React</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent/>)
---
+++ dist/Namaste React.7c0ccee6.js
@@ -0,0 +1,12 @@
+const header = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "Hello World"),
+    React.createElement('h2', {}, "Hello World 2")
+]));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(header);
+
+//# sourceMappingURL=Namaste React.7c0ccee6.js.map
